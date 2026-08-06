85ebb4b1fdf27f9fbcca3c9f149b5fb2733801df
mmugambiwa14-netizen <mmugambiwa14@gmail.com>
2026-08-07 01:19:45 +0300

    Remove duplicate verified business notification rollback

--- supabase/rollback/20260807011000_verified_business_notifications_and_profile_bootstrap.rollback.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-drop trigger if exists notify_business_category_state on public.business_category_approvals;
-drop trigger if exists notify_business_application_state on public.business_applications;
-drop function if exists public.notify_business_application_state();
-drop trigger if exists bootstrap_verified_business_profile on public.business_profiles;
-drop function if exists public.bootstrap_business_profile_verification();
-
-commit;
