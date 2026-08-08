17bd7ccdb99e709268ae3ce51a088bf01ab82406
mmugambiwa14-netizen <mmugambiwa14@gmail.com>
2026-08-08 14:52:01 +0300

    Make listing schema contract Node-portable

--- src/services/listingSubmissionContracts.js
+++ src/services/listingSubmissionContracts.js
@@ -1,4 +1,4 @@
-import { toStoragePayload } from '@/services/listingAttributes';
+import { toStoragePayload } from './listingAttributes.js';
 
 const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const SLUG_PATTERN = /^[a-z0-9]+(?:_[a-z0-9]+)*$/;
